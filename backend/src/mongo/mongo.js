99e89d067eff7e330b06d6f78a739dbab0d28180
CBorum <christopherborum@gmail.com>
2018-05-11 15:12:38 +0200

    bra queries og testefar

--- backend/src/mongo/mongo.js
+++ backend/src/mongo/mongo.js
@@ -10,8 +10,15 @@ const dbConnectionOpts = {
 };
 
 exports.connect = () => {
-    mongoose.connect(process.env.MONGO_URL, dbConnectionOpts).
-        catch((err) => {
+    let url = 'mongodb://localhost:27017/gutenberg'
+    if (process.env.MONGO_URL && process.env.NODE_ENV !== 'test') {
+        url = process.env.MONGO_URL
+    }
+    return mongoose.connect(url, dbConnectionOpts)
+        .then(() => {
+            console.log('connected to ' + url)
+        })
+        .catch((err) => {
             console.log(err);
         });
 };
@@ -20,9 +27,9 @@ exports.disconnect = () => mongoose.disconnect();
 
 exports.getCities = () => City.find({}).limit(10);
 
+// Given a city name your application returns all book titles with corresponding authors that mention this city.
 // Returns promise with books that mention the cityName
 exports.getBooksMetionCity = (cityName) => new Promise((resolve, reject) => {
-
     // Can return multiple cities with same name
     City.find({ cityName })
         .then(async (cities) => {
@@ -36,30 +43,78 @@ exports.getBooksMetionCity = (cityName) => new Promise((resolve, reject) => {
             resolve(allBooks);
         })
         .catch(reject);
-
 });
 
-exports.nearGeo = (lng, lat) => {
+// Given a book title, your application plots all cities mentioned in this book onto a map.
+exports.getCitiesFromBook = (title) => new Promise((resolve, reject) => {
+    Book.aggregate([
+        { $match: { title } },
+        { $unwind: '$cityRefs' },
+        {
+            $lookup:
+                {
+                    from: 'cities',
+                    localField: 'cityRefs',
+                    foreignField: 'cityId',
+                    as: 'city',
+                }
+        },
+        { $project: { 'city': true, '_id': false } }
+    ])
+        .then(resolve)
+        .catch(reject)
+})
 
-    const maxDistance = 10000;
+// Given an author name your application lists all books written by that author and plots all cities mentioned in any of the books onto a map.
+exports.getCitiesAndBooksFromAuthor = (author) => new Promise((resolve, reject) => {
+    Book.aggregate([
+        { $match: { authors: author } },
+        { $unwind: '$cityRefs' },
+        {
+            $lookup:
+                {
+                    from: 'cities',
+                    localField: 'cityRefs',
+                    foreignField: 'cityId',
+                    as: 'city',
+                }
+        },
+    ])
+        .then(resolve)
+        .catch(reject)
+})
+
+// Given a geolocation, your application lists all books mentioning a city in vicinity of the given geolocation.
+exports.getBooksNearLocation = (lng, lat) => new Promise((resolve, reject) => {
+    const maxDistance = 100000;
     const limit = 10;
 
-    mongoose.connection.db.collection('cities').aggregate([
+    City.aggregate([
         {
-            '$geoNear': {
-                'near': {
-                    'coordinates': [
-                        lat,
-                        lng
+            $geoNear: {
+                near: {
+                    coordinates: [
+                        lng,
+                        lat
                     ],
-                    'type': 'Point'
+                    type: 'Point'
                 },
                 distanceField: 'dist.calculated',
                 maxDistance,
                 num: limit,
                 spherical: true
             }
+        },
+        {
+            $lookup:
+                {
+                    from: 'books',
+                    localField: 'cityId',
+                    foreignField: 'cityRefs',
+                    as: 'books',
+                }
         }
-    ]);
-
-};
+    ])
+        .then(resolve)
+        .catch(reject)
+});
--- backend/tests/bookup.test.js
+++ backend/tests/bookup.test.js
@@ -1,13 +1,12 @@
 const mongo = require('../src/mongo/mongo');
-
 require('dotenv').load();
 
 beforeAll(() => {
-    mongo.connect();
+    return mongo.connect();
 });
 
 afterAll(() => {
-    mongo.disconnect();
+    mongo.disconnect()
 });
 
 test('get cities', async () => {
@@ -22,7 +21,7 @@ test('get cities', async () => {
         });
 });
 
-test('get books that reference paris', async () => {
+test('get books that reference Rome', async () => {
     await mongo.getBooksMetionCity('Rome')
         .then((res) => {
             expect(res.length).toBe(6)
@@ -31,3 +30,42 @@ test('get books that reference paris', async () => {
             expect(err).toBeNull();
         });
 });
+
+test('get cities from The Federalist Papers book', async () => {
+    await mongo.getCitiesFromBook('The Federalist Papers')
+        .then((res) => {
+            console.log(res)
+            if (res[0] && res[0].city[0]) {
+                console.log(res[0].city[0])
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+test('get books and cities from J. Sheridan LeFanu author', async () => {
+    await mongo.getCitiesAndBooksFromAuthor('J. Sheridan LeFanu')
+        .then((res) => {
+            console.log(res)
+            if (res[0]) {
+                console.log(res[0])
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+test('get books from neaby cities to location', async () => {
+    await mongo.getBooksNearLocation(-95, 33)
+        .then((res) => {
+            console.log(res)
+            if (res[0]) {
+                console.log(res[0])
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
