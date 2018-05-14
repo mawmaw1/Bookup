28f45ed5f63e6ea0718b78565d2302b24408c64e
xNoga <k.noga@hotmail.com>
2018-05-14 17:22:51 +0200

    lorte dist for at undgå conflict

--- backend/dist/postgres/postgres.js
+++ backend/dist/postgres/postgres.js
@@ -1,24 +1,31 @@
 const { Client } = require('pg')
-
+require('dotenv').load();
 const client = new Client({
-    user: 'postgres',
-    host: '212.47.237.59',
-    database: 'postgres',
-    password: 'kukenrbra',
-    port: 7655,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PW,
+    port: process.env.POSTGRES_PORT,
 })
 
 client.connect()
 
+client.on('error', (err) => {
+  console.error('something bad has happened!', err.stack)
+})
+
 exports.query1 = (title) => {
     const res = client.query(`
-        select Book.title, Author.name FROM Book
-        join Book_City USING (bookid)
-        join Book_Author using (bookid)  
-        join City using (cityid)
-        join Author using (authorid)  
-        WHERE City.name = '${title}'    
+        SELECT DISTINCT Book.bookid, Book.title, array_agg(Author.name) from Book
+        JOIN Book_City using (bookid) 
+        JOIN Book_Author using (bookid) 
+        JOIN City using (cityid) 
+        JOIN Author using (authorid) 
+        WHERE City.name = '${title}' 
+        GROUP BY Book.bookid
+        ORDER BY Book.title; 
     `)
+
     return res
 }
 
