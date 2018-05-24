0fb80a88fca13b4bdff3c7bea795dba889399135
kristian94 <kristian.o.g.nielsen@gmail.com>
2018-05-24 17:32:42 +0200

    coverage paa frontens ende

--- frontend/index.js
+++ frontend/index.js
@@ -10,7 +10,12 @@ const app = express();
 const port = process.argv[2] || 8080;
 const ip = process.argv[3] || '0.0.0.0';
 
-app.use('/', express.static(path.join(__dirname, 'dist')));
+app.get('/', (req, res) => {
+    res.redirect('/app')
+});
+
+app.use('/app', express.static(path.join(__dirname, 'dist')));
+app.use('/coverage', express.static(path.join(__dirname, 'tests/coverage/lcov-report')));
 
 app.listen(port, ip, function(){
     console.log(`'dist' served on ${ip}:${port}`)
