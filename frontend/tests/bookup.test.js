c71eb5f154a49ac945cfedc180c94582f52791a6
Magnus Larsen <ml@schantz.com>
2018-06-03 17:11:06 +0200

    øget ventetid pga langsom mongo

--- frontend/tests/bookup.test.js
+++ frontend/tests/bookup.test.js
@@ -8,11 +8,11 @@ const timeoutMs = 4000
 beforeAll(async () => {
     try {
 
-        driver = await new Builder().forBrowser('chrome').usingServer('http://localhost:4444/wd/hub').build();
-        //driver = await new Builder().forBrowser('chrome').build()
+        //driver = await new Builder().forBrowser('chrome').usingServer('http://localhost:4444/wd/hub').build();
+        driver = await new Builder().forBrowser('chrome').build()
         
         await driver.get(process.env.FRONTEND_URL || 'http://localhost:8080/')
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
     }
     catch (e) {
         console.log(e)
